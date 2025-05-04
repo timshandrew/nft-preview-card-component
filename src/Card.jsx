16836d177e1dbd0aca661e418bab5aed54bc8ae6
Andrew <andrew.alan.sm@gmail.com>
2025-05-04 12:29:24 +0100

    add custom colours wth Tailwind themes

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,20 +3,22 @@ import avatarImg from "/images/image-avatar.png";
 
 export default function Card() {
   return (
-    <div className="w-2xs rounded-2xl bg-gray-700 p-5 text-white">
+    <div className="w-2xs rounded-2xl bg-teal-500 p-5 text-teal-400 shadow-2xl shadow-teal-700">
       <img
         className="mb-4 rounded-2xl"
         src={productImage}
         alt="Etherem product image"
       />
-      <h1 className="mb-4 text-xl font-semibold">Equilibrium #3429</h1>
+      <h1 className="mb-4 text-xl font-semibold text-white">
+        Equilibrium #3429
+      </h1>
       <p className="mb-4 text-gray-400">
         Our Equilibrium collection promotes balance and calm.
       </p>
       <div className="flex justify-between border-b-2 border-teal-600 pb-4">
         <div className="flex gap-2">
           <img src="./images/icon-ethereum.svg" alt="Ethereum icon" />
-          <span className="w-max font-semibold text-teal-300">0.041 ETH</span>
+          <span className="text-turqoise w-max font-semibold">0.041 ETH</span>
         </div>
         <div className="flex gap-2">
           <img src="images/icon-clock.svg" alt="Clock icon" />
@@ -30,7 +32,7 @@ export default function Card() {
           alt="Creator avatar"
         />
         <span className="mx-auto">
-          <span className="text-gray-300">Creation of</span> Jules Wyvern
+          Creation of <span className="text-white">Jules Wyvern</span>
         </span>
       </div>
     </div>
