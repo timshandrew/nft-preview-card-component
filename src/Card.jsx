68ee9b20a7f657f689a973f2dded54ae6722e079
Andrew <andrew.alan.sm@gmail.com>
2025-05-04 11:12:25 +0100

    further styling

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,26 +3,34 @@ import avatarImg from "/images/image-avatar.png";
 
 export default function Card() {
   return (
-    <div className="w-2xs bg-gray-700 p-5 text-white">
-      <img src={productImage} alt="Etherem product image" />
-      <h1 className="mb-2 text-xl">Equilibrium #3429</h1>
-      <p className="mb-2 text-gray-400">
+    <div className="w-2xs rounded-2xl bg-gray-700 p-5 text-white">
+      <img
+        className="mb-4 rounded-2xl"
+        src={productImage}
+        alt="Etherem product image"
+      />
+      <h1 className="mb-4 text-xl font-semibold">Equilibrium #3429</h1>
+      <p className="mb-4 text-gray-400">
         Our Equilibrium collection promotes balance and calm.
       </p>
-      <div className="mb-2 flex justify-between">
-        <div className="flex">
+      <div className="flex justify-between border-b-2 border-teal-600 pb-4">
+        <div className="flex gap-2">
           <img src="./images/icon-ethereum.svg" alt="Ethereum icon" />
-          <span className="text-teal-300">0.041 ETH</span>
+          <span className="w-max font-semibold text-teal-300">0.041 ETH</span>
         </div>
-        <div className="flex">
+        <div className="flex gap-2">
           <img src="images/icon-clock.svg" alt="Clock icon" />
           <span className="text-gray-400">3 days left</span>
         </div>
       </div>
-      <div className="flex items-center">
-        <img className="size-12" src={avatarImg} alt="Creator avatar" />
+      <div className="flex items-center pt-4">
+        <img
+          className="size-9 rounded-full border-1 border-white"
+          src={avatarImg}
+          alt="Creator avatar"
+        />
         <span className="mx-auto">
-          Creation of <span>Jules Wyvern</span>
+          <span className="text-gray-300">Creation of</span> Jules Wyvern
         </span>
       </div>
     </div>
